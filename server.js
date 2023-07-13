abe52028db0714a75c9b5401ba432d89b550d2ee
Stanislav <bassketb86@gmail.com>
2023-07-13 18:39:40 +0300

    !

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
+
 
 // const path = require('path');
 
@@ -26,4 +28,13 @@ app.use(express.json())
     
 app.listen(3001, () => {
   console.log("run on port 3001");
+});
+
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 });
